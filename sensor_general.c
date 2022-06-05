33ad9d787adeca944a6ada76e3f508b20f7eb2b4
Rio Liu <rio.liu@r26.me>
2022-06-05 13:49:06 -0400

    update sensor and pressure conversion

--- sensor_general.c
+++ sensor_general.c
@@ -7,6 +7,8 @@
 
 #include "sensor_general.h"
 
+const float VREF = 3.3;
+
 void LED_init(void) {
     TRISB4 = 0;     //set B4 as output
     LATB4 = 1;      // turn the led off
@@ -25,42 +27,35 @@ void LED_heartbeat(void) {
 
 uint32_t get_pressure_psi(void) {
     adc_result_t voltage_raw = ADCC_GetSingleConversion(channel_PRESSURE);
-    return voltage_raw; // For debugging purposes only
 
+    float v = (voltage_raw + 0.5f) / 4096.0f * VREF;
     
-    // Resistor divider maps 0-5V output to 0-1.5V
-    int voltage_5v = voltage_raw * 5 / 6;
+    const double r = 99.8;
+    const double pressure_range = 3000;
 
-    // No longer valid: 
-    // Scaling factors taken from (for P1):
-    // https://docs.google.com/spreadsheets/d/1NJDRvIkPtVGVRjQt-zyT8ZoKv0JL28MJ4FAHNd4Z_rc/edit#gid=0
-    // Now, it's a 4-20 mA sensor going through a 100 ohm resistor. This is for 0-3000psi
-    // TF 0.4-2V now.
-    
-    int32_t pressure_add = (0); 
-    const double voltage_range = 1.6;
-    const double pressure_scale = (3000)/(voltage_range);
-    const double minimum_voltage = 0.4;
-    
-    double voltage_adj = voltage_raw - minimum_voltage;
+    double current = v / r;
 
-    int32_t pressure_psi = (int32_t) (voltage_adj * pressure_scale + pressure_add);
+    int32_t pressure_psi = (int32_t) ((current - 0.004) / (0.02 - 0.004) * pressure_range);
     //if (pressure_psi < 0) { pressure_psi = 0; }
 
-    return (uint32_t)pressure_psi;
+    return (uint32_t) pressure_psi;
 }
 
 
 uint16_t get_temperature_c(void) {
     adc_result_t voltage_raw = ADCC_GetSingleConversion(channel_THERM);
 
+    const float rdiv = 10000.0; // 10kohm dividor resistor
+
+    // beta, r0, t0 from https://media.digikey.com/pdf/Data%20Sheets/Thermometrics%20Global%20Business%20PDFs/TG_Series.pdf
     const float beta = 3434.0;
-    const float invBeta = 1.0 / beta;
-    const float adcMax = 4096.0;
-    const float invT0 = 1.0 / 298.15; // room temp in Kelvin
-    float K, C;
-    K = 1.0 / (invT0 + invBeta*(log(adcMax / (float) voltage_raw - 1.0)));
-    C = K - 273.15; // convert to Celsius
+    const float r0 = 10000.0;
+    const float t0 = 298.15;   // 25 C in Kelvin
+
+    float v = (voltage_raw + 0.5f) / 4096.0f * VREF; // (raw + contiuity correction) / 12bit adc * vref
+    float r = v * rdiv / (VREF - v);
+
+    float invk = 1 / t0 + 1 / beta * log (r / r0);
   
-    return (uint16_t) C;
+    return (uint16_t) (1/invk);
 }
