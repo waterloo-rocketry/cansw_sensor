e4f08abec2dda7e1dcff68dc6ed8fc2d787588bc
artemis.sotnikov12@gmail.com <artemis.sotnikov12@gmail.com>
2022-06-05 00:12:10 -0400

    Change ADC conversion constants
    Code complies but is not functional yet, something is wrong with the ADC conversions. Debugging must occur.

--- sensor_general.c
+++ sensor_general.c
@@ -24,18 +24,27 @@ void LED_heartbeat(void) {
 
 uint32_t get_pressure_psi(void) {
     adc_result_t voltage_raw = ADCC_GetSingleConversion(channel_PRESSURE);
-    return voltage_raw;
+    return voltage_raw; // For debugging purposes only
 
+    
     // Resistor divider maps 0-5V output to 0-1.5V
     int voltage_5v = voltage_raw * 5 / 6;
 
+    // No longer valid: 
     // Scaling factors taken from (for P1):
     // https://docs.google.com/spreadsheets/d/1NJDRvIkPtVGVRjQt-zyT8ZoKv0JL28MJ4FAHNd4Z_rc/edit#gid=0
-    int32_t pressure_add = (40); // adjusted scaling values from june 12th
-    double pressure_scale = (621.55265);
+    // Now, it's a 4-20 mA sensor going through a 100 ohm resistor. This is for 0-3000psi
+    // TF 0.4-2V now.
+    
+    int32_t pressure_add = (0); 
+    const double voltage_range = 1.6;
+    const double pressure_scale = (3000)/(voltage_range);
+    const double minimum_voltage = 0.4;
+    
+    double voltage_adj = voltage_raw - minimum_voltage;
 
-    int32_t pressure_psi = (int32_t) voltage_5v * pressure_scale / (1000) + pressure_add;
-    if (pressure_psi < 0) { pressure_psi = 0; }
+    int32_t pressure_psi = (int32_t) (voltage_adj * pressure_scale + pressure_add);
+    //if (pressure_psi < 0) { pressure_psi = 0; }
 
     return (uint32_t)pressure_psi;
 }
